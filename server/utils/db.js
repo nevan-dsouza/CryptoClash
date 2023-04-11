abb1f0c86cfbdbc7dd76541a817edc61e9a23683
Nevan D'Souza <51455256+nevan-dsouza@users.noreply.github.com>
2023-04-11 04:33:49 -0400

    changed uri for mongodb connection

--- server/utils/db.js
+++ server/utils/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // console.log(process.env.DB_URI);
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nevan-dsouza:dkTVqsEL8YR2Uamb@cyrptoclash.frozmvu.mongodb.net/?retryWrites=true&w=majority";
+const uri = "placeholder_URI";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("room");
